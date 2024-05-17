50a2faf40e2290bc0ee9311a27ee4ee9d6058819
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-05-17 13:28:01 -0300

    change folder's

--- projeto_locadora/index.php
+++ projeto_locadora/index.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Locadora</title>
+    <title>Locadora 🚗</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
 </head>
-<body>
 
-    
+<body>
+    <!-- <iframe src="../html/fragmentos/header.html" frameborder="0"></iframe> -->
+    <?php include 'html/header.html'; ?>
+        <div class="banner">
+           <img src="../img/Frame 1.png" alt="">
+        </div>
+
 </body>
+
 </html>
